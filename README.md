e61489c97b81b46cbffa83bf8ff7fa06fac88eb2
Aditya Rana <81512209+ar7aditya@users.noreply.github.com>
2021-09-11 15:58:04 +0530

    blog3.0 with database

---
+++ README.md
@@ -0,0 +1,6 @@
+ <h3> Upcomming Upgrade plans </h3> 
+1. This project needs upgradation and connection to database. <br>
+2. further it needs to be intergrated with a todolist project. <br>
+3. introduction of authentication. <br>
+4. introduction to chatbox. <br>
+5. introduction to public declaration and user browsing. <br>
---
+++ app.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const path = require("path");
+const ejs = require("ejs");
+const _ = require('lodash');
+const port = 8080;
+let posts = [];
+let myArray = Object.values(posts);
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'))
+
+// mongoose.connect("mongodb://localhost:27017/blogDBpost", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://wwb_bc:www.wwb3.0@cluster0.q2hi3.mongodb.net/WWBlog?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
+
+const postSchema = {
+  title: String,
+  content: String
+};
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", (req, res) => {
+    Post.find({}, function(err, posts){
+    res.render('home.ejs', {
+        posts: posts
+    });
+})
+});
+app.get("/compose", (req, res) => {
+    res.render('compose.ejs');
+})
+
+app.post('/compose', (req, res) => {
+    const post = new Post({
+        title: req.body.postTitle,
+        content: req.body.postBody
+    });
+    
+    post.save();
+    res.redirect("/");
+
+});
+
+app.get('/posts/:userId', function (req, res) {
+    let requestedTitle = _.lowerCase(req.params.userId);
+    
+    Post.find((err,posts)=>{
+        if(err){
+            console.log(err);
+        }else{
+    posts.forEach((i) => {
+        const storedTitle = _.lowerCase(i.title);
+
+   if (storedTitle === requestedTitle) {
+            res.render('post.ejs', {
+                title: i.title,
+                content: i.content
+
+            });
+        }
+    });
+}
+});
+});
+
+app.get("/about", (req, res) => {
+    res.render('about.ejs');
+})
+app.get("/contact", (req, res) => {
+    res.render('contact.ejs');
+})
+
+app.listen(process.env.PORT || port,()=>{
+    console.log(`The application started successfully on port ${port}`);
+});
+
