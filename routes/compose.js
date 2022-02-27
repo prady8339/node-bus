38f0e41530f0e0b21300f6f97fdad8fe37e89aad
prady8339 <76277136+prady8339@users.noreply.github.com>
2022-02-28 02:59:24 +0530

    time && user info

--- routes/compose.js
+++ routes/compose.js
@@ -1,5 +1,5 @@
 
-module.exports = function(app,Post){
+module.exports = function(app,Post,User){
   
     app.get("/compose", (req, res) => {
     if (req.isAuthenticated()) {
@@ -9,14 +9,47 @@ module.exports = function(app,Post){
               }
             })
        app.post('/compose', (req, res) => {
-        const post = new Post({
+
+
+
+        User.find({ "_id": { $eq:req.user.id} }, function (err, foundUsers) {
+          if (err) {
+            console.log(err);
+          } else {
+
+            const today = new Date();
+            
+            const monthNames = ["January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"];
+
+            const sufix = ["st","nd","rd","th"];
+            let mysufix = "";
+            if(today.getDate()%10-1<4)
+             mysufix = [(today.getDate()%10-1)];
+            else
+            mysufix = "th";
+
+            const date =today.getDate()+ mysufix + ' '+(monthNames[today.getMonth()]) +' '+today.getFullYear();
+            const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    
+          const post = new Post({
           title: req.body.postTitle,
           content: req.body.postBody,
-          UserId:req.user.id
+          date:date,
+          time:time,
+          UserId:foundUsers[0]._id,
+          username:foundUsers[0].username
         });
-      
+  
         post.save();
         res.redirect("/");
+
+
+      }
+
+
+      })
+    
       
       });
             
@@ -24,3 +57,8 @@ module.exports = function(app,Post){
     
          
     }
+
+    
+
+
+
